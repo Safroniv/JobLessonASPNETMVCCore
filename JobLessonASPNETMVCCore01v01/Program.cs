b46bc75f8f19b44ac5b5e55e0abbe1a99b5ce330
Иван Сафронов <safroniv@mail.ru>
2022-12-03 15:45:16 +0300

    JobLessonASPNETMVCCore fin003

--- JobLessonASPNETMVCCore01v01/Program.cs
+++ JobLessonASPNETMVCCore01v01/Program.cs
@@ -1,20 +1,23 @@
 ﻿
-
-
-
 Task1();
-
 Task2();
-
+Console.WriteLine("\nВывод сделанный после выполнения:" +
+    "\n Разделение выполнения операций по потокам ускоряет выполнение кода программы" +
+    "\n пропорционально количеству создаваемых потоков!");
 
 ///<summary>
 ///Метод использует один поток
 ///</summary>
 static void Task1()
 {
-
-    float[] arr = new float[100_000_000];
     DateTime start = DateTime.Now;
+    Console.WriteLine($"Метод Task1() выполняется в контексте потока {Thread.CurrentThread.ManagedThreadId}");
+    float[] arr = new float[100_000_000];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = 1;
+    }
+
     for (int i = 0; i < arr.Length; i++)
     {
         arr[i] = (float)(arr[i] * Math.Sin(0.2f + i / 5) * Math.Cos(0.2f + i / 5) * Math.Cos(0.4f + i / 2));
@@ -28,6 +31,8 @@ static void Task1()
 ///</summary>
 static void Task2()
 {
+    DateTime start = DateTime.Now;
+    Console.WriteLine($"Метод Task2() выполняется в контексте потока {Thread.CurrentThread.ManagedThreadId}");
     float[] arr = new float[100_000_000];
 
     int mid1 = (arr.Length + 1) / 2;
@@ -67,13 +72,28 @@ static void Task2()
     {
         Console.WriteLine($"Метод Task2()  thread4 выполняется в контексте потока {Thread.CurrentThread.ManagedThreadId}");
         for (int i = 0; i < firstHalf2.Length; i++)
+        {
+            firstHalf2[i] = (float)(arr[i] * Math.Sin(0.2f + i / 5) * Math.Cos(0.2f + i / 5) * Math.Cos(0.4f + i / 2));
+        }
+    });
+    thread4.Start();
+
+    float[] secondHalf2 = arr.Skip(mid2).ToArray();
+    Console.WriteLine(secondHalf2.Length);
+    //создание второго потока
+    Thread thread5 = new Thread(() =>
     {
-        arr[i] = 1;
-    }
-    for (int i = 0; i < arr.Length; i++)
-    {
-        arr[i] = (float)(arr[i] * Math.Sin(0.2f + i / 5) * Math.Cos(0.2f + i / 5) * Math.Cos(0.4f + i / 2));
-    }
+        Console.WriteLine($"Метод Task2()  thread5 выполняется в контексте потока {Thread.CurrentThread.ManagedThreadId}");
+        for (int j = 0; j < secondHalf2.Length; j++)
+        {
+            secondHalf2[j] = (float)(arr[j] * Math.Sin(0.2f + j / 5) * Math.Cos(0.2f + j / 5) * Math.Cos(0.4f + j / 2));
+        }
+    });
+    thread5.Start();
+    //соединение подмассивов в новый массив
+    float[] newArr2 = firstHalf2.Concat(secondHalf2).ToArray();
+    Console.WriteLine($"размер сшитого массива = {newArr2.Length}");
+
 
     DateTime finish = DateTime.Now;
 

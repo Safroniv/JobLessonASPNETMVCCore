d54ea5d5d0dc7bd73903f59b3580b468c1176255
Иван Сафронов <safroniv@mail.ru>
2022-12-03 14:36:11 +0300

    Red001

--- JobLessonASPNETMVCCore01v01/Program.cs
+++ JobLessonASPNETMVCCore01v01/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Task1();
+
+Task2();
+
+
+///<summary>
+///Метод использует один поток
+///</summary>
+static void Task1()
+{
+
+    float[] arr = new float[100_000_000];
+    DateTime start = DateTime.Now;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = (float)(arr[i] * Math.Sin(0.2f + i / 5) * Math.Cos(0.2f + i / 5) * Math.Cos(0.4f + i / 2));
+    }
+    DateTime finish = DateTime.Now;
+    Console.WriteLine($"Инициализация массива заняла у нас: {finish - start} сек.");
+}
+
+static void Task2()
+{
+    float[] arr = new float[100_000_000];
+    DateTime start = DateTime.Now;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = (float)(arr[i] * Math.Sin(0.2f + i / 5) * Math.Cos(0.2f + i / 5) * Math.Cos(0.4f + i / 2));
+    }
+
+    DateTime finish = DateTime.Now;
